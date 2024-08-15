a4f15f85386e79d6277077a065722814b1c63507
Raythan Padovani Abreu Machado <raythan100@yahoo.com.br>
2024-08-15 10:46:22 -0300

    scripts

--- vBot48_basic.lua
+++ vBot48_basic.lua
@@ -422,4 +422,16 @@ UI.TextEdit(storage.autoBlessMessage or "!bless", function(widget, text)
   storage.autoBlessMessage = text
 end)
 
+macro(400, "Cavebot Check", function()
+    if player:getHealth() == 0 and CaveBot.isOn() then
+		CaveBot.setOff()
+		print("Cavebot desligado: Personagem morreu.")
+    end
+    
+    if isInPz() and stamina() < 18 * 60 and CaveBot.isOn() then  -- stamina() retorna a stamina em minutos
+		CaveBot.setOff()
+		print("Cavebot desligado: Stamina abaixo de 18:00 e personagem em PZ.")
+    end
+end)
+
 UI.Separator()
